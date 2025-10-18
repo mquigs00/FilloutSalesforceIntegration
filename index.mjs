2b07eb5ebb3d435e7d1ad4771556e437672d7951
Matt Quigley <mquigs00@gmail.com>
2025-10-18 14:00:48 -0400

    Added functionality to create an Account, and Create Contacts. Trigger in Salesforce will link Contacts to Account.

--- index.mjs
+++ index.mjs
@@ -17,11 +17,42 @@ async function getPrivateKey() {
             throw new Error("Secret String for private key is empty or undefined");
         }
 
-        console.log("Received response for Salesforce private key");
-
         return response.SecretString;
     } catch (error) {
         console.error("Error retrieving Salesforce private key");
+        throw error;
+    }
+}
+
+/**
+ * Prints all of the Field Names and Field Labels that are available for the given Salesforce Objects API
+ * 
+ * @param {String} objectName - the name of the Salesforce Object (ex "Lead", "Contact", "Account")
+ * @param {JSON} tokenData - the JSON webtoken
+ * @returns {JSON} - response.data
+ */
+async function verifySalesforceSchema(objectName, tokenData) {
+    try {
+        const instanceUrl = tokenData.instance_url || 'https://orgfarm-b10ac254af-dev-ed.develop.my.salesforce.com';
+
+        const response = await axios.get(
+            `${instanceUrl}/services/data/v61.0/sobjects/${objectName}/describe`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${tokenData.access_token}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+
+        console.log(`Fields for ${objectName}`);
+        response.data.fields.forEach(field => {
+            console.log(`${field.name}: ${field.label}`);
+        })
+
+        return response.data;
+    } catch (error) {
+        console.error("Error verifying Salesforce schema:", error);
         throw error;
     }
 }
@@ -43,7 +74,6 @@ async function getSalesforceSecrets() {
             throw new Error("Secret String is empty or undefined");
         }
 
-        console.log("Received response for Salesforce credentials");
         const secrets = JSON.parse(response.SecretString);
         
         const privateKey = await getPrivateKey();
@@ -76,8 +106,6 @@ async function getSalesforceAccessToken() {
         const username = salesforceCredentials.username;
         const loginUrl = salesforceCredentials.loginUrl;
 
-        console.log("Login URL: " + loginUrl + " Username = " + username);
-
         const jwtQuery = {
             iss: consumerKey,
             sub: username,
@@ -86,8 +114,6 @@ async function getSalesforceAccessToken() {
         };
 
         const signedJWT = jwt.sign(jwtQuery, reformattedPrivateKey, {algorithm: 'RS256'});
-
-        console.log(signedJWT);
 
         const res = await axios.post(
             `${loginUrl}/services/oauth2/token`,
@@ -111,6 +137,12 @@ async function getSalesforceAccessToken() {
     }
 };
 
+/**
+ * Gets the corresponding Salesforce State Code for the given state name
+ * 
+ * @param {String} state - the full name of the state
+ * @returns {String} stateDict[state] - the corresponding Salesforce State Code
+ */
 function getStateCode(state) {
     const stateDict = {
         "Alabama": "AL",
@@ -178,45 +210,221 @@ function getStateCode(state) {
     return stateDict[state]
 }
 
-async function insertLead(clientData, tokenData) {
-    console.log("Inserting lead");
-    const instanceUrl = tokenData.instance_url || 'https://orgfarm-b10ac254af-dev-ed.develop.my.salesforce.com';
-    console.log("State: " + clientData.address.stateCode);
-    const response = await axios.post(
-        `${instanceUrl}/services/data/v61.0/sobjects/Lead`,
-        {
-            FirstName: clientData.personal.firstName,
-            LastName: clientData.personal.lastName,
-            Street: clientData.address.streetAddress,
-            City: clientData.address.city,
-            PostalCode: clientData.address.zipcode,
-            CountryCode: "US",
-            StateCode: clientData.address.stateCode,
-            Phone: clientData.contact.phone,
-            Email: clientData.contact.email,
-            Primary_Language__c: clientData.contact.primaryLanuage,
-            Household_Size__c: clientData.household.householdSize,
-            Estimated_Monthly_Household_Income__c: clientData.household.householdMonthlyIncome,
-            Company: "Self",
-            Status: "Open - Not Contacted"
-        },
-        {
-            headers: {
-                'Authorization': `Bearer ${tokenData.access_token}`,
-                'Content-Type': 'application/json'
+/**
+ * Checks if a Lead already exits in Salesforce account with the same first name, last name, and email address
+ * 
+ * @param {Object} clientData 
+ * @param {JWT Object} tokenData 
+ * @returns duplicateLeadExists - boolean indicating whether a duplicate lead exists
+ */
+async function duplicateLeadExits(clientData, tokenData) {
+    try {
+        const instanceUrl = tokenData.instance_url || 'https://orgfarm-b10ac254af-dev-ed.develop.my.salesforce.com';
+
+        const selectDuplicateQuery = `
+            SELECT FirstName, LastName, Email
+            FROM Lead
+            WHERE FirstName = '${clientData.personal.firstName}'
+            AND LastName = '${clientData.personal.lastName}'
+            AND Email = '${clientData.contact.email}'
+        `;
+
+        const response = await axios.get(
+            `${instanceUrl}/services/data/v61.0/query/?q=${encodeURIComponent(selectDuplicateQuery)}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${tokenData.access_token}`
+                }
             }
+        );
+
+        let duplicateLeadExists = false;
+
+        if (response.data.records.length > 0) {
+            duplicateLeadExists = true;
         }
-    )
+
+        return duplicateLeadExists;
+    } catch (error) {
+        console.error("Error checking for duplicate lead:", error);
+        throw error;
+    }
+    
+}
+
+/**
+ * Creates a new Lead in Salesforce
+ * 
+ * @param {JSON} clientData 
+ * @param {JSON} tokenData 
+ * @returns 
+ */
+async function insertLead(clientData, tokenData) {
+    try {
+        console.log("Inserting lead");
+        const instanceUrl = tokenData.instance_url || 'https://orgfarm-b10ac254af-dev-ed.develop.my.salesforce.com';
+        console.log("State: " + clientData.address.stateCode);
+        const response = await axios.post(
+            `${instanceUrl}/services/data/v61.0/sobjects/Lead`,
+            {
+                FirstName: clientData.personal.firstName,
+                LastName: clientData.personal.lastName,
+                Street: clientData.address.streetAddress,
+                City: clientData.address.city,
+                PostalCode: clientData.address.zipcode,
+                CountryCode: "US",
+                StateCode: clientData.address.stateCode,
+                Phone: clientData.contact.phone,
+                Email: clientData.contact.email,
+                Primary_Language__c: clientData.contact.primaryLanguage,
+                Family_Type__c: clientData.household.familyType,
+                Housing_Status__c: clientData.household.housingStatus,
+                Household_Size__c: clientData.household.householdSize,
+                Estimated_Monthly_Household_Income__c: clientData.household.householdMonthlyIncome,
+                Date__c: clientData.dateOfSubmission,
+                Company: "Self",
+                Status: "Closed - Converted",
+                LeadSource: "Fillout"
+            },
+            {
+                headers: {
+                    'Authorization': `Bearer ${tokenData.access_token}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        return response.data.id;
+    } catch (error) {
+        console.error("Error inserting lead:", error.response.data);
+        throw error;
+    };
 };
 
+/**
+ * 
+ * @param {JSON} accountData - all the Fillout answers that pertain to the Account
+ * @param {String} leadId - the 18 digit ID of the Lead
+ * @param {JSON} tokenData
+ * 
+ * @returns response.data.id - the Account ID of the newly created Account
+ */
+async function createAccount(accountData, leadId, tokenData) {
+    try {
+        const instanceUrl = tokenData.instance_url || 'https://orgfarm-b10ac254af-dev-ed.develop.my.salesforce.com';
+
+        const response = await axios.post(
+            `${instanceUrl}/services/data/v61.0/sobjects/Account`,
+            {
+                Name: `${accountData.firstName} ${accountData.lastName} Household`,
+                Primary_Language__c: accountData.primaryLanguage,
+                Family_Type__c: accountData.familyType,
+                Housing_Status__c: accountData.housingStatus,
+                Address__Street__s: accountData.address.streetAddress,
+                Address__City__s: accountData.address.city,
+                Address__PostalCode__s: accountData.address.zipcode,
+                Address__CountryCode__s: "US",
+                Address__StateCode__s: accountData.address.stateCode,
+                Lead_ID__c: leadId,
+                Source__c: "Fillout"
+            },
+            {
+                headers: {
+                    'Authorization': `Bearer ${tokenData.access_token}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        return response.data.id;
+    } catch (error) {
+        console.error("Error creating Account: ", error.response.data);
+        throw error;
+    }
+}
+
+/**
+ * Creates a new Salesforce Contact for each household member entered in the Fillout form
+ * 
+ * @param {List} householdMembers - the list of household member objects
+ * @param {String} leadId - the 18 digit ID of the Lead
+ * @param {String} accountId - the Account ID that the Contacts will belong to
+ * @param {JSON} tokenData 
+ */
+async function insertHouseholdMembers(householdMembers, leadId, accountId, tokenData) {
+    console.log("Starting insertHouseholdMembers");
+    const instanceUrl = tokenData.instance_url || 'https://orgfarm-b10ac254af-dev-ed.develop.my.salesforce.com';
+    console.log("Instance URL: " + instanceUrl);
+
+    // Fillout form uses a Yes/No answer for disability questions. Map them to the Salesforce checkbox/boolean options
+    const disabledMap = {
+        "Yes": true,
+        "No": false
+    }
+
+    try {
+        let phone, email;
+        for (let i = 0; i < householdMembers.length; i++) {
+            console.log(`Household member ${i+1}: ${householdMembers[i].firstName} ${householdMembers[i].lastName}`)
+            if (i == 0) {
+                phone = householdMembers[0].phoneNumber;
+                email = householdMembers[0].emailAddress;
+            }
+
+            console.log("Making axios call");
+
+            const response = await axios.post(
+                `${instanceUrl}/services/data/v61.0/sobjects/Contact`,
+                {
+                    FirstName: householdMembers[i].firstName,
+                    LastName: householdMembers[i].lastName,
+                    Relationship_to_Head_of_Household__c: householdMembers[i].relationship,
+                    Birthdate: householdMembers[i].birthdate,
+                    Gender_Identity__c: householdMembers[i].genderIdentity,
+                    Race__c: householdMembers[i].race,
+                    Hispanic_Status__c: householdMembers[i].ethnicity,
+                    Highest_Education_Completed__c: householdMembers[i].highestEducationCompleted,
+                    Marital_Status__c: householdMembers[i].maritalStatus,
+                    Military_Status__c: householdMembers[i].militaryStatus,
+                    Health_Insurance_Coverage__c: householdMembers[i].healthInsuranceCoverage,
+                    Disabled__c: disabledMap[householdMembers[i].isDisabled],
+                    AccountId: accountId,
+                    Lead_ID__c: leadId,
+                    Source__c: "Fillout"
+                },
+                {
+                    headers: {
+                        'Authorization': `Bearer ${tokenData.access_token}`,
+                        'Content-Type': 'application/json'
+                    }
+                }
+            )
+        }
+    } catch(error) {
+        if (error.response) {
+            console.error("Salesforce API error:", error.response.status, error.response.data);
+        } else if (error.request) {
+            console.error("No response received from Salesforce API:", error.request);
+        } else {
+            console.error("Error creating Contact: ", error.message);
+        }
+        throw error;
+    }
+}
+
+/**
+ * 
+ * @param {List} questions 
+ * @param {String} questionName - the question text ("Member 2 Is Disabled?")
+ * @returns 
+ */
 function getAnswerForQuestion(questions, questionName) {
-    const questionObj = questions.find(question => question.name === questionName);
+    const questionObj = questions.find(question => question.name === questionName);         // find the question with the given name
 
     if (!questionObj) {
         throw new Error(`Question with name ${questionName} not found`);
     }
 
-    return questionObj.value;
+    return questionObj.value;                                                               // value is the user's answer to the question
 };
 
 export const handler = async (event) => {
@@ -225,20 +433,62 @@ export const handler = async (event) => {
     const body = JSON.parse(event.body);
     let questions = body.submission.questions;
 
+    const filloutId = body.submission.submissionId;
+
     const firstName = getAnswerForQuestion(questions, "First Name");
     const lastName = getAnswerForQuestion(questions, "Last Name");
     const email = getAnswerForQuestion(questions, "Email");
-    const phone = getAnswerForQuestion(questions, "Phone");
+    const phone = getAnswerForQuestion(questions, "Phone Number");
     const address = getAnswerForQuestion(questions, "Your address");
-    const primaryLanuage = getAnswerForQuestion(questions, "Primary Language");
-    const householdSize = getAnswerForQuestion(questions, "Number of family members in your household");
+    const primaryLanguage = getAnswerForQuestion(questions, "Primary Language");
+    const housingStatus = getAnswerForQuestion(questions, "Housing Status");
+    const householdSize = getAnswerForQuestion(questions, "Number of members in your household");
+    const familyType = getAnswerForQuestion(questions, "Family Type");
     const householdMonthlyIncome = getAnswerForQuestion(questions, "Estimated Monthly Household Income");
+    const dateOfSubmission = getAnswerForQuestion(questions, "Date");
 
     const streetAddress = address.address;
     const city = address.city;
     const state = address.state;
     const stateCode = getStateCode(state);
     const zipcode = address.zipcode;
+
+    let householdMembers = [];
+    let client = {}
+    client.firstName = firstName;
+    client.lastName = lastName;
+    client.relationship = "Head of Household";
+    client.phone = phone;
+    client.email = email;
+    client.birthdate = getAnswerForQuestion(questions, "Birthdate");
+    client.genderIdentity = getAnswerForQuestion(questions, "Gender Identity");
+    client.maritalStatus = getAnswerForQuestion(questions, "Marital Status");
+    client.race = getAnswerForQuestion(questions, "Race");
+    client.highestEducationCompleted = getAnswerForQuestion(questions, "Highest Education Completed");
+    client.militaryStatus = getAnswerForQuestion(questions, "Military Status");
+    client.ethnicity = getAnswerForQuestion(questions, "Ethnicity");
+    client.isDisabled = getAnswerForQuestion(questions, "Disabled?");
+    client.healthInsuranceCoverage = getAnswerForQuestion(questions, "Health Insurance Coverage");
+    
+    householdMembers.push(client);
+
+    // add any additional househld members to the list
+    for (let i = 2; i <= householdSize; i++) {
+        let nextMember = {}
+        nextMember.firstName = getAnswerForQuestion(questions, `Person ${i} First Name`);
+        nextMember.lastName = getAnswerForQuestion(questions, `Person ${i} Last Name`);
+        nextMember.birthdate = getAnswerForQuestion(questions, `Person ${i} Birthdate`);
+        nextMember.relationship = getAnswerForQuestion(questions, `Person ${i} Relationship to You`);
+        nextMember.genderIdentity = getAnswerForQuestion(questions, `Person ${i} Gender Identity`);
+        nextMember.maritalStatus = getAnswerForQuestion(questions, `Person ${i} Marital Status`);
+        nextMember.race = getAnswerForQuestion(questions, `Person ${i} Race`);
+        nextMember.highestEducationCompleted = getAnswerForQuestion(questions, `Person ${i} Highest Education Completed`);
+        nextMember.militaryStatus = getAnswerForQuestion(questions, `Person ${i} Military Status`);
+        nextMember.ethnicity = getAnswerForQuestion(questions, `Person ${i} Ethnicity`);
+        nextMember.isDisabled = getAnswerForQuestion(questions, `Is Person ${i} Disabled?`);
+        nextMember.healthInsuranceCoverage = getAnswerForQuestion(questions, `Person ${i} Health Insurance Coverage`);
+        householdMembers.push(nextMember);
+    }
 
     console.log("Extracted all form data");
 
@@ -250,7 +500,7 @@ export const handler = async (event) => {
         contact: {
             email,
             phone,
-            primaryLanuage
+            primaryLanguage
         },
         address: {
             streetAddress,
@@ -260,18 +510,48 @@ export const handler = async (event) => {
             zipcode
         },
         household: {
+            housingStatus,
             householdSize,
-            householdMonthlyIncome
-        }
+            familyType,
+            householdMonthlyIncome,
+        },
+        dateOfSubmission
     };
+
+    const accountData = {
+        firstName,
+        lastName,
+        primaryLanguage,
+        housingStatus,
+        familyType,
+        address: {
+            streetAddress,
+            city,
+            state,
+            stateCode,
+            zipcode
+        }
+    }
 
     const tokenData = await getSalesforceAccessToken();
 
-    console.log("Retrieved signed JWT token")
+    if (await duplicateLeadExits(clientData, tokenData)) {
+        console.log("Duplicate lead exists");
+        return {
+            statusCode: 200,
+            body: JSON.stringify('Duplicate Lead Exists!'),
+        };
+    }
 
-    await insertLead(clientData, tokenData);
+    console.log("No duplicate exists adding lead");
+    const leadId = await insertLead(clientData, tokenData);
+    console.log("Lead inserted with id: " + leadId);
 
-    console.log("Passed insert lead");
+    await verifySalesforceSchema("Account", tokenData);
+
+    const accountId = await createAccount(accountData, leadId, tokenData);
+
+    await insertHouseholdMembers(householdMembers, leadId, accountId, tokenData);
 
     const response = {
         statusCode: 200,
